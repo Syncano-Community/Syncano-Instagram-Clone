92751eb85804714d871d8e9dd1209a19a898a051
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-11-18 10:11:59 +0100

    Implement splash screen.

---
+++ app/src/main/java/com/solid9studio/instagram/screen/splashScreen/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.solid9studio.instagram.screen.splashScreen;
+
+import android.os.Bundle;
+
+import com.solid9studio.instagram.BaseActivity;
+import com.solid9studio.instagram.R;
+import com.solid9studio.instagram.screen.loginScreen.LoginActivity;
+import com.solid9studio.instagram.screen.postListScreen.PostListActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (isLoggedIn()) {
+            startActivity(PostListActivity.getActivityIntent(this));
+        } else {
+            startActivity(LoginActivity.getActivityIntent(this));
+        }
+    }
+
+    private boolean isLoggedIn() {
+        // TODO Add login check logic.
+        return true;
+    }
+}
