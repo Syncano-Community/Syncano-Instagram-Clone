1ceafafd8250759f5382ccce0d9f2bef4e1ebff9
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-11-18 14:37:18 +0100

    Layout for settings.

--- app/src/main/java/com/solid9studio/instagram/screen/settingsScreen/SettingsActivity.java
+++ app/src/main/java/com/solid9studio/instagram/screen/settingsScreen/SettingsActivity.java
@@ -3,6 +3,9 @@ package com.solid9studio.instagram.screen.settingsScreen;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.solid9studio.instagram.BaseActivity;
 import com.solid9studio.instagram.R;
@@ -18,5 +21,27 @@ public class SettingsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sign_out:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void signOut() {
+
     }
 }
