29a1dd4a72019f817fc5f7e2250aa2873866dfb6
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-11-22 10:22:42 +0100

    Update settings activity.

--- app/src/main/java/com/solid9studio/instagram/screen/settingsScreen/SettingsActivity.java
+++ app/src/main/java/com/solid9studio/instagram/screen/settingsScreen/SettingsActivity.java
@@ -3,14 +3,23 @@ package com.solid9studio.instagram.screen.settingsScreen;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Switch;
 
 import com.solid9studio.instagram.BaseActivity;
 import com.solid9studio.instagram.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SettingsActivity extends BaseActivity {
+
+    @BindView(R.id.switch_notifications)
+    Switch notificationsSwitch;
 
     public static Intent getActivityIntent(Context context) {
         Intent intent = new Intent(context, SettingsActivity.class);
@@ -21,6 +30,7 @@ public class SettingsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -33,15 +43,20 @@ public class SettingsActivity extends BaseActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_sign_out:
-                signOut();
+            case R.id.action_save:
+                saveChanges();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void signOut() {
+    public void saveChanges() {
+        Log.d("SettingsActivity", "saveChanges notifications: " + notificationsSwitch.isChecked());
+    }
 
+    @OnClick(R.id.sign_out_button)
+    public void signOut() {
+        Log.d("SettingsActivity", "signOut");
     }
 }
